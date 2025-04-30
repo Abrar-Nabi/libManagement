c477a984e240e021160ea8a594085a609fa94533
abrarnabi <117633785+Abrar-Nabi@users.noreply.github.com>
2025-04-30 11:23:42 +0530

    Update user-dashboard.js

--- client/src/pages/user-dashboard.js
+++ client/src/pages/user-dashboard.js
@@ -86,7 +86,8 @@ const UserDashboard = () => {
                     <td>{b.bookId?.author}</td>
                     <td>{new Date(b.createdAt).toLocaleDateString()}</td>
                     <td>{days}</td>
-                    <td>₹{days * 10}</td>
+          <td>₹{Math.max(0, days - 9) * 10}</td>
+
                     <td>{b.returned ? "✅ Returned" : " Not Returned"}</td>
                   </tr>
                 );
